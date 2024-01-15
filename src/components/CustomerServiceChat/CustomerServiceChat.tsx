c9436a3e723f03bbec1b78784c9b5362e8479752
Onimae <manufelipearias@gmail.com>
2024-01-15 22:58:07 +1100

    created chat component, created chat header and chat input for customers

--- src/components/CustomerServiceChat/CustomerServiceChat.tsx
+++ src/components/CustomerServiceChat/CustomerServiceChat.tsx
@@ -1,10 +1,36 @@
 import { FC } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "../ui/accordion";
+import ChatHeader from "./ChatHeader";
+import ChatInput from "./ChatInput";
 
 const CustomerServiceChat: FC = () => {
   return (
-    <div>
-      <h1>Welcome to CustomerServiceChat!</h1>
-    </div>
+    <Accordion
+      type="single"
+      collapsible
+      className="relative bg-white z-40 shadow"
+    >
+      <AccordionItem value="item-1">
+        <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden ">
+          <div className="w-full h-full flex flex-col">
+            <AccordionTrigger className="px-6 border-b bg-slate-300 ">
+              <ChatHeader />
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col h-80">
+                messages
+                <ChatInput className="px-4 " />
+              </div>
+            </AccordionContent>
+          </div>
+        </div>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
