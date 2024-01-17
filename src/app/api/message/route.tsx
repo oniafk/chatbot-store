b4ef49567e237bfe1009d08e0f66ddd441734e90
Onimae <manufelipearias@gmail.com>
2024-01-17 23:55:11 +1100

    post request to openAI API with prompt and messages types and roles

--- src/app/api/message/route.tsx
+++ src/app/api/message/route.tsx
@@ -1,3 +1,19 @@
+import { ChatGPTMessage } from "app/components/lib/openAI/openai-stream";
+import { MessageArraySchema } from "app/components/lib/schemas/message.schema";
+import { chatbotPrompt } from "app/helpers/constants/chatbot.prompt";
+
 export async function POST(req: Request) {
-  console.log(" end point works");
+  const { messages } = await req.json();
+
+  const parsedMessages = MessageArraySchema.parse(messages);
+
+  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
+    role: message.isUserMessage ? "user" : "system",
+    content: message.text,
+  }));
+
+  outboundMessages.unshift({
+    role: "system",
+    content: chatbotPrompt,
+  });
 }
