88a402eb654d32560e0aac3600d9c0a4fe774d07
Onimae <manufelipearias@gmail.com>
2024-01-19 01:02:26 +1100

    fixing styles on chat messages, improving styles on chat input and disable meanwhile answer is being generated by llm

--- src/components/CustomerServiceChat/ChatMessages.tsx
+++ src/components/CustomerServiceChat/ChatMessages.tsx
@@ -31,24 +31,23 @@ const ChatMessages: FC<ChatMessageProps> = ({ className, ...props }) => {
               className={cn(
                 "flex flex-col space-y-2 text-sm max-w-xs mx-2 overflow-x-hidden",
                 {
-                  "bg-blue-600 text-white": message.isUserMessage,
-                  "bg-gray-200 text-gray-900": !message.isUserMessage,
+                  "order-1 item-end": message.isUserMessage,
+                  "order-2 items-start": !message.isUserMessage,
                 }
               )}
             >
               <p
-                className={cn("px-4 py2 rounded-lg", {
+                className={cn("px-4 py-2 rounded-lg", {
                   "bg-blue-600 text-white": message.isUserMessage,
                   "bg-gray-200 text-gray-900": !message.isUserMessage,
                 })}
-              ></p>
-              <MarkdownLite text={message.text} />
-              {message.text}
+              >
+                <MarkdownLite text={message.text} />
+              </p>
             </div>
           </div>
         </div>
       ))}
-      ChatMessage
     </div>
   );
 };
